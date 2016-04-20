fae0e62d17a57d50ea08f150c270504c73e8e5cd
Naomi Lockhart <naomi@lockyy.com>
2016-04-20 02:55:23 +0100

    Add seeds to db/seeds.rb for setting up database
    This'll let you mess with the API immediately

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+subject = Subject.create(name: 'RPG')
+
+generator = Generator.create(name: 'Character Generator', subject: subject)
+
+c1 = generator.column.create(name: 'column 1', min: 1, max: 2, chance_of_multiple: 50)
+c1_o1 = c1.options.create(text: 'column 1 option 1')
+c1_o2 = c1.options.create(text: 'column 1 option 2')
+c1_o2_c1 = c1_o2.columns.create(name: 'column 1 option 2 column 1')
+c1_o2_c1_o1 = c1_o2_c1.options.create(text: 'column 1 option 2 column 1 option 1')
+c2 = c1.columns.create(name: 'column 2')
+c2_o1 = c2.options.create(text: 'column 2 option 1')
