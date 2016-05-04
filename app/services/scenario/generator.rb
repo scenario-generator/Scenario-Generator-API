91cdf98ef57688e63ee7b0496b995af20f367124
Naomi Lockhart <naomi@lockyy.com>
2016-05-05 00:32:52 +0100

    Make columns nested in API response
    This will allow us to more easily reroll specific columns

--- app/services/scenario/generator.rb
+++ app/services/scenario/generator.rb
@@ -14,25 +14,28 @@ module Scenario
     def process_column(column)
       columns = []
 
-      # Add the current column and it's opens to the array
       options = column.pick
-      columns << {
-        column: column,
-        options: options
-      }
 
+      child_columns = []
       # If the options we picked have columns then we need to recursively add their columns to the array
       # Doing it here ensures that it appears in the correct place in the scenario
       options.each do |option|
         option.columns.each do |column|
-          columns += process_column(column)
+          child_columns += process_column(column)
         end
       end
 
       # If this column has any child columns, recursively add them to the array as well
       column.columns.each do |column|
-        columns += process_column(column)
+        child_columns += process_column(column)
       end
+
+      # Add the current column, it's options, and any child columns to the array
+      columns << {
+        column: column,
+        options: options,
+        child_columns: child_columns,
+      }
 
       return columns
     end
--- app/views/api/v1/scenarios/_column.json.jbuilder
+++ app/views/api/v1/scenarios/_column.json.jbuilder
@@ -1,2 +1,3 @@
 json.(column[:column], :id, :name)
 json.options column[:options], partial: 'api/v1/scenarios/option', as: :option
+json.columns column[:child_columns], partial: 'api/v1/scenarios/column', as: :column
