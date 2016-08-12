71fa67f8275adf932ef8d89b5adbd94a3431aec5
Naomi Lockhart <naomi@lockyy.com>
2016-08-12 23:30:01 +0100

    Remove login system

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules.
-  devise :database_authenticatable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
-  include DeviseTokenAuth::Concerns::User
-end
