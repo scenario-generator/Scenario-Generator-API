1e075aa99d67ed6e412aa4e753ded53e3349c159
Naomi Lockhart <naomi@lockyy.com>
2017-08-07 18:55:57 +0100

    Improve efficiency of scenario importing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,12 @@ puts "Generators imported"
 total_scenarios = EXPORTED_SCENARIOS.length
 puts "Importing #{total_scenarios} scenarios"
 
-EXPORTED_SCENARIOS.each_with_index do |exported_scenario, i|
-  params = Importing::ScenarioParams.from(exported_scenario)
-  puts "#{i}/#{total_scenarios}"  if i % 1000 == 0
-  Scenario.create(params)
+EXPORTED_SCENARIOS.each_slice(100) do |scenarios_slice|
+  scenarios_slice.each do |exported_scenario|
+    next if Scenario.find_by(uuid: exported_scenario[:uuid]).present?
+    params = Importing::ScenarioParams.from(exported_scenario)
+    Scenario.create(params)
+  end
 end
 puts "Scenarios imported"
+
